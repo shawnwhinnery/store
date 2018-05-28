8bdfc2b481282f7535c1cbd2a557a93e4ecf533b
shawn <shawnsdrive@gmail.com>
2018-05-27 18:55:58 -0700

    bugfix

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 const _ = require('lodash')
 
 class Store {
-    
-    constructor (initialState, reducers, options) {
+
+    constructor(initialState, reducers, options) {
         this.state = _.clone(initialState)
         this.reducers = reducers
         this.subscribers = new Set([])
-    }
+        this.dispatch = (action) => {
 
-    dispatch (action) {
-        
-        if(Array.isArray(action)) return action.forEach(dispatch)
-        
-        var reducer = _.get(this.reducers, action.type)
-        
-        if(typeof reducer === 'function') {
-            this.state = reducer(this.getState(), action, this.dispatch)
-            this.subscribers.forEach( fn => fn(this.state) )
+            if (Array.isArray(action)) return action.forEach(this.dispatch)
+
+            var reducer = _.get(this.reducers, action.type)
+
+            if (typeof reducer === 'function') {
+                this.state = reducer(this.getState(), action, this.dispatch)
+                this.subscribers.forEach(fn => fn(this.state))
+            }
+
         }
-
     }
 
-    getState () {
+
+
+    getState() {
         return _.clone(this.state)
     }
 
-    subscribe (fn) {
-        if(!this.subscribers.has(fn))
+    subscribe(fn) {
+        if (!this.subscribers.has(fn))
             this.subscribers.add(fn)
     }
 
-    unsubscribe (fn) {
+    unsubscribe(fn) {
         this.subscribers.remove(fn)
     }
 
